87b1a486d947bb49e2a6e8f1f3fd28b8702f080a
Jose Flores <jose.flores.carmona@gmail.com>
2023-06-27 01:16:40 +0200

    new main file

---
+++ terraform/main.tf
@@ -0,0 +1,13 @@
+Terraform {
+    required_providers {    // proveedores de azure de esta fuente harshicop de version 3.02 o superior del proveedor
+        azurerm = {
+            source = "hashicorp/azurerm"
+            version = "~> 3.0.2"
+        }
+    }
+    required_version = ">= 1.1.0"   // version de terraform 1.1.0 o superior
+}
+
+Provider "azurerm" {  // version expefica para otras features, salvo casos puntuales no se añada nada
+features{}
+}
