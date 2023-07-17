22642bf0fbc70da2931654c706976a6db6fdd49d
Jose Flores <jose.flores.carmona@gmail.com>
2023-07-17 23:23:45 +0200

    misspelling

--- terraform/single_vm.tf
+++ terraform/single_vm.tf
@@ -19,7 +19,7 @@ resource "azurerm_subnet" "subnet" { // definir la subred
   address_prefixes     = ["10.0.2.0/24"]
 }
 
-esource "azurerm_public_ip" "pip" {
+resource "azurerm_public_ip" "pip" {
   name                = "VIP"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
